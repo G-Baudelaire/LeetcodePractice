775137223db9fcc8d065aa5b2e6bd698a20e5e80
Germain Jones <germainjones1@gmail.com>
2024-11-13 14:35:04 +0000

    Change 71 Solution

--- 71.py
+++ 71.py
@@ -1,34 +1,26 @@
-import re
-# Looking at solutions this can be done much more simply using .split and .join
-
-class Solution(object):
-    def simplifyPath(self, path):
-        """
-        :type path: str
-        :rtype: str
-        """
-        left_pointer = 1
+class Solution:
+    def simplifyPath(self, path: str) -> str:
         stack = []
-
-        path = re.sub(r"/+", "/", path)
-        while left_pointer < len(path):
-            match = re.match("[0-9A-z._]+", path[left_pointer:])
-
-            if match.group() is None:
-                left_pointer += 1
-                continue
-            if match.group() == ".":
+        term_generator = self.get_next_term(path)
+        for term in term_generator:
+            if not term or term == ".":
                 pass
-            elif match.group() == "..":
-                stack = stack[:-1]
-            elif match.group():
-                stack += [match.group()]
+            elif term == "..":
+                try:
+                    stack.pop()
+                except IndexError:
+                    pass
+            else:
+                stack.append(term)
 
-            left_pointer += match.end() + 1
+        return "/" + "/".join(stack)
 
-        output = "/" if not stack else ""
-
-        for s in stack:
-            output += "/" + s
-
-        return output
+    def get_next_term(self, path):
+        substring = ""
+        for char in path:
+            if char == "/":
+                yield substring
+                substring = ""
+            else:
+                substring += char
+        yield substring
